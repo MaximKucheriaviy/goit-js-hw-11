051f54b4f4ff77b2adeabb403ccd7a8690058115
Maxim Kucheriaviy <maxim-k@i.ua>
2022-08-09 22:39:06 +0300

    getready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './css/styles.css';
-import pixabayReqest from './js/pixabayGet';
+import pixabayGet from './js/pixabayGet';
 
 const options = {
     key: "29155901-7a6502b1ec64ba72602e491fa",
-    q: "yellow flovers"
+    q: "yellow flowers"
 }
 
-pixabayReqest(options)
+pixabayGet(options)
 .then(data => {
     console.log(data);
 })
--- src/js/pixabayGet.js
+++ src/js/pixabayGet.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 
 const url = 'https://pixabay.com/api/';
 
-export default  function pixabayReqest({q, key, imageType = "photo", orientation = "horizontal", safesearch = true} = {}){
+export default async function pixabayGet({q, key, imageType = "photo", orientation = "horizontal", safesearch = true, perPage = 40, page = 1} = {}){
     q = q.split("");
     q = q.map(item => item === " " ? "+" : item).join("");
-    const reqUrl = `${url}?key=${key}&q=${q}&imageType=${imageType}&o&safesearch=${safesearch}`
-    return  axios.get(reqUrl);
+    const reqUrl = `${url}?key=${key}&q=${q}&imageType=${imageType}&o&safesearch=${safesearch}&per_page=${perPage}&page=${page}`;
+    try{
+        const response = await axios.get(reqUrl);
+        return response.data
+    }
+    catch(err){
+        console.log(err);
+    } 
 }
