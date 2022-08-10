b09f99632c1f1d4a52443be0541218d5bf730a8f
Maxim Kucheriaviy <maxim-k@i.ua>
2022-08-10 11:41:04 +0300

    markup

--- src/js/createPhotoDiv.js
+++ src/js/createPhotoDiv.js
@@ -1,6 +1,11 @@
-export default function createPhotoDiv({webformatURL = "", largeImageURL = "", tags = "", likes = "", views = "", comments = "", downloads = ""} = {}){
+export default function createPhotoDiv({webformatURL = "", largeImageURL = "", tags = "", likes = "", views = "", comments = "", downloads = "", webformatHeight, webformatWidth} = {}){
+    const width = Math.round(webformatWidth * 192 / webformatHeight);
+    //console.log(webformatWidth, webformatHeight);
+    const imageStyle = webformatHeight < 480 ? `style="max-width: none; width: ${width}px; height: 192px"` : "";
     const photoDiv = `<div class="photo-card" data-large-url="${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+    <div class="image-thumb">
+    <img src="${webformatURL}" alt="${tags}" ${imageStyle} loading="lazy"/>
+    </div>
     <div class="info">
     <p class="info-item">
         <b>Likes</b> ${likes}
